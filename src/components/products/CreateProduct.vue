71956b79a5e77f0f23d16b5c868c09659a645f2a
FarzanaHQTech <farzana.tech24@gmail.com>
2025-04-15 13:10:47 +0600

    product create

--- src/components/products/CreateProduct.vue
+++ src/components/products/CreateProduct.vue
@@ -1,41 +1,77 @@
-<script setup>
-import { reactive } from 'vue';
-import { useProductStore } from '@/store/ProductStore';
-import { useRouter } from 'vue-router';
 
-const productStore = useProductStore();
+
+<script setup>
+import { useProductStore } from '@/store/ProductStore';
+import { onMounted, reactive } from 'vue';
+// import { useProductStore } from '@/store/ProductStore';
+import { useRouter } from 'vue-router';
+// import { api } from '@/api';
+import { toast } from 'vue3-toastify';
+
+
+const productJoinStore = useProductStore()
+// const productStore = useProductStore();
 const router = useRouter()
 const productData = reactive({
-	name: "",
-	product_type_id: "",
-	category_id: "",
-	squ: "",
-	qty: "",
-	uom_id: "",
-	size_id: "",
-	unit_price: ""
+    name: "",
+    product_type_id: "",
+    category_type_id: "",
+    sku: "",
+    qty: "",
+    uom_id: "",
+    size_id: "",
+    unit_price: ""
 });
 
-const handleSubmit = async () => {
-	try {
-		await productStore.createProduct(productData);
-		alert("Product created successfully!");
-		// Optionally reset the form
-		Object.keys(productData).forEach(key => productData[key] = "");
-        router.push("products")
-	} catch (error) {
-		alert("Failed to create product. Check console.");
-	}
+onMounted(async()=>{
+  await productJoinStore.fetchProducts('/products');
+})
+// const categories = ref([]);
+// const productTypes = ref([]);
+// const sizes = ref([]);
+// const uoms = ref([]);
 
+// const productsJoinData = useProductStore()
+// console.log(productsJoinData )
+// onMounted(async()=>{
+//     await productsJoinData.fetchProducts('/products')
    
+// })
+
+
+const handleSubmit = async()=>{
+    try {
+        const res = await api.post("/products",productData)
+        console.log(res.data)
+        if(res.data.status===200){
+            toast.success(res.data.message)
+            Object.keys(productData).forEach(key=>{
+                productData[key]= "";
+            })
+            router.push("/products")
+        }
+    } catch (error) {
+        console.log("create product failed:", error);
+        alert("Failed to create product: " + (error.response?.data?.message || error.message));
+    }
 }
+
+// const handleSubmit = async () => {
+//     try {
+//         await productStore.createProduct(productData);
+//         alert("Product created successfully!");
+//         // Optionally reset the form
+//         Object.keys(productData).forEach(key => productData[key] = "");
+//         router.push("/products")
+//     } catch (error) {
+//         alert("Failed to create product. Check console.");
+//     }
+// }
 </script>
-
-
 <template>
     <div class="max-w-4xl  mx-auto bg-white p-8 shadow-md rounded-md mt-10">
         <h2 class="text-2xl text-center font-bold mb-5">Add Product</h2>
-       
+
         <form @submit.prevent="handleSubmit">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <!-- Name -->
@@ -43,10 +79,20 @@ const handleSubmit = async () => {
                     <label class="block text-sm font-medium text-gray-700 mb-1">Product Name</label>
                     <div class="relative">
                         <input v-model="productData.name" type="text"
-                            class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600"
-                            placeholder="Enter your name" />
+                            class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600" />
                         <span class="absolute right-3 top-2.5 text-gray-400">
                             <i class="fas fa-user"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div>
+                    <label class="block text-sm font-medium text-gray-700 mb-1">SKU</label>
+                    <div class="relative">
+                        <input type="text" v-model="productData.sku"
+                            class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600" />
+                        <span class="absolute right-3 top-2.5 text-gray-400">
+                            <i class="fas fa-eye-slash"></i>
                         </span>
                     </div>
                 </div>
@@ -54,84 +100,83 @@ const handleSubmit = async () => {
                 <!-- Email -->
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Product Type</label>
-                    <div class="relative">
-                        <input  v-model="productData.product_type_id" type="text"
-                            class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600"
-                            placeholder="Enter your email" />
-                        <span class="absolute right-3 top-2.5 text-gray-400">
-                            <i class="fas fa-envelope"></i>
-                        </span>
-                    </div>
-                </div>
-
-                <!-- Password -->
-                <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-1">Product Size</label>
-                    <select  v-model="productData.size_id"
+                    <select v-model="productData.product_type_id"
+                  
+                   
                         class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600">
-                        <option value="">Select User Role</option>
+                        <option value="">Select product type</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
                     </select>
                 </div>
 
-              
-
+                <!-- Password -->
                 <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-1">SQU</label>
-                    <div class="relative">
-                        <input type="text"  v-model="productData.squ"
-                            class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600"
-                            placeholder="Confirm password" />
-                        <span class="absolute right-3 top-2.5 text-gray-400">
-                            <i class="fas fa-eye-slash"></i>
-                        </span>
-                    </div>
-                </div>
-
-            
-                <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-1">Product Category</label>
-                    <select  v-model="productData.category_id"
+                    <label class="block text-sm font-medium text-gray-700 mb-1">Product Size</label>
+                    <select v-model="productData.size_id"
                         class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600">
-                        <option value="">Select Category</option>
+                        <option value="">Select Size</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </select>
+                </div>
+                <div>
+                    <label class="block text-sm font-medium text-gray-700 mb-1">Category Type</label>
+                    <select v-model="productData.category_type_id"
+                        class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600">
+                        <option value="">Select Category Type</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
                     </select>
                 </div>
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Unit of Measure</label>
-                    <select  v-model="productData.uom_id"
+                    <select v-model="productData.uom_id"
                         class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600">
                         <option value="">Select User UOM</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
                     </select>
                 </div>
-                
+
                 <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-1">Unit price</label>
+                    <label class="block text-sm font-medium text-gray-700 mb-1">Quantity</label>
                     <div class="relative">
-                        <input type="text"  v-model="productData.unit_price"
-                            class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600"
-                            placeholder="Enter password" />
+                        <input type="text" v-model="productData.qty"
+                            class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600" />
                         <span class="absolute right-3 top-2.5 text-gray-400">
                             <i class="fas fa-eye-slash"></i>
                         </span>
                     </div>
                 </div>
-              
+                <div>
+                    <label class="block text-sm font-medium text-gray-700 mb-1">Unit price</label>
+                    <div class="relative">
+                        <input type="text" v-model="productData.unit_price"
+                            class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600" />
+                        <span class="absolute right-3 top-2.5 text-gray-400">
+                            <i class="fas fa-eye-slash"></i>
+                        </span>
+                    </div>
+                </div>
+
                 <!-- Submit -->
-                <div class="mt-6">
+                <div class="md:col-span-2 mt-6">
                     <button type="submit"
                         class="w-full bg-purple-800 text-white py-2 px-4 rounded hover:bg-purple-900 transition duration-200">
                         Add product
                     </button>
                 </div>
-                </div>
+            </div>
         </form>
     </div>
- 
+
 </template>
 
 <style lang="scss" scoped></style>
+
+
+
+
+
+
--- src/store/ProductStore.js
+++ src/store/ProductStore.js
@@ -1,7 +1,6 @@
-
-
 // store/ProductStore.js
 import { api } from '@/api';
+import Category from '@/pages/Category.vue';
 import { Search } from 'lucide-vue-next';
 import { defineStore } from 'pinia';
 // import api from '@/axios'; // assuming you're using axios instance
@@ -12,7 +11,8 @@ export const useProductStore = defineStore('product', {
 		search: null,
 		error: null,
 		loading: false,
-		product: null
+		product: null,
+		categories:[],
 	}),
 
 	actions: {
@@ -21,6 +21,7 @@ export const useProductStore = defineStore('product', {
 			try {
 				const response = await api.get(url, { params: { search: this.search } });
 				this.products = response.data.products.data;
+				// this.categories = response.data.products
 				console.log("Response data:", response.data);
 			} catch (error) {
 				this.error = error;
@@ -30,17 +31,17 @@ export const useProductStore = defineStore('product', {
 		}
 	},
 
-	// store/ProductStore.js
-	async createProduct(data) {
-		try {
-			const res = await api.post('/products', data);
-			this.product = res.data;
-		} catch (error) {
-			console.log('create error', error);
-			this.error = error;
-			throw error;
-		}
-	}
+	// // store/ProductStore.js
+	// async createProduct(data) {
+	// 	try {
+	// 		const res = await api.post('/products', data);
+	// 		this.product = res.data;
+	// 	} catch (error) {
+	// 		console.log('create error', error);
+	// 		this.error = error;
+	// 		throw error;
+	// 	}
+	// }
 
 });
 
